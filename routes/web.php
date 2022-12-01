5024440d1fc7af0a225967f516d56d12b86cf58d
Viacheslav Tykhonov <viache86@gmail.com>
2022-12-01 23:09:28 +0200

    search added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use \App\Models\Category;
 use \App\Models\Post;
 use \App\Models\User;
+use \App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,26 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-//    $posts = cache()->remember("/", 300 , function() {
-//       return Post::all();});
-
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-        ]);
-})->name('home');
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', ['post'=> $post]);
-});
 Route::get('categories/{category:slug}', function (Category $category) {
     return view(
         'posts', [
             'posts' => $category->posts,
             'currentCategory' => $category,
             'categories' => Category::all()
-        ])->name('categories');
+        ]);
 });
 Route::get('authors/{author:username}', function (User $author) {
     return view(
